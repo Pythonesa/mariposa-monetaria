66f8ab764bf7bdbe2dcd375916666e6a258ed27e
Pythonesa <pythonesamar@gmail.com>
2024-01-08 16:38:37 -0300

    :lipstick: header

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
-import './App.css'
+import Logo from "./assets/images/logo.svg"
 
 function App() {
 
   return (
-    <>
-      
-    </>
+    <div className="App min-h-screen flex flex-col bg-mm-sand">
+      <div className="flex flex-col bg-mm-blue">
+        <div className="header flex flex-row justify-between items-center">
+          <div className="flex flex-row justify-center items-center mx-4">
+            <img src={Logo} alt="Mariposa Monetaria Logo" className="w-32"/>
+            <h1 className="text-4xl font-bold text-mm-yellow ">Mariposa Monetaria</h1>
+          </div>
+          <nav className="flex flex-row justify-center items-center p-4 text-mm-sand text-2xl font-bold ">
+            <button className="mx-4 text-mm-sand  hover:text-mm-yellow transition-colors duration-500 ease-in-out border-b-2 border-transparent hover:border-mm-yellow">📋 Nuevo registro</button>
+            <button className="mx-4 text-mm-sand  hover:text-mm-yellow transition-colors duration-500 ease-in-out border-b-2 border-transparent hover:border-mm-yellow">📊 Ver historial</button>
+          </nav>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'mm-orange': '#de6600',
+        'mm-yellow': '#fea02f',
+        'mm-sand': '#ebd9c8',
+        'mm-teal': '#007a7a',
+        'mm-blue': '#003f5a',
+      }
+    },
   },
   plugins: [],
 }
