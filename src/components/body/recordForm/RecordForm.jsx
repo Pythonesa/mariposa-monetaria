7fae4ef70b5966cb0547d37b7ec174dd20959437
Pythonesa <pythonesamar@gmail.com>
2024-01-14 17:06:17 -0300

    :lipstick: add isIncome field in form

--- src/components/body/recordForm/RecordForm.jsx
+++ src/components/body/recordForm/RecordForm.jsx
@@ -24,11 +24,18 @@ export default function RecordForm() {
   return (
     <form name="recordForm" className="flex flex-col justify-center items-center w-80 sm:w-10/12">
       <RecordFormItem labelText={"Nombre:"} inputName={"name"} inputPlaceHolder={"Ej. Compra supermercado"} onChange={handleInputChange} inputValue={record.name}/>
+      <div className="flex flex-col p-2 w-11/12 sm:flex-row">
+        <label htmlFor="isIncome" className="text-mm p-1 text-left mr-2 sm:w-1/3 sm:text-right">Tipo</label>
+        <select name="isIncome" className="text-mm p-1 w-full rounded placeholder-gray-300 bg-white/80" onChange={handleInputChange}>
+          <option value="false">Gasto</option>
+          <option value="true">Ingreso</option>
+        </select>
+      </div>
       <RecordFormItem labelText={"Monto:"} inputName={"amount"} inputType={"number"} onChange={handleInputChange} inputValue={String(record.amount)}/>
       <RecordFormItem labelText={"Categoría:"} inputName={"category"} inputPlaceHolder={"Ej. supermercado"} onChange={handleInputChange} inputValue={record.category}/>
       <RecordFormItem labelText={"Descripción:"} inputName={"description"} inputPlaceHolder={"Ej. Frutas y verduras para el mes."} onChange={handleInputChange}/>
       <RecordFormItem labelText={"Fecha:"} inputName={"date"} inputType={"date"} inputValue={record.date} onChange={handleInputChange}/>
-      <button type="submit" className="p-2 m-2 bg-mm-blue text-mm-yellow rounded shadow shadow-mm hover:bg-mm-teal hover:text-mm-yellow" onClick={hadleFormSubmit}>Añadir registro</button>
+      <button type="submit" className="p-2 m-2 bg-mm-blue text-mm-yellow rounded shadow shadow-mm hover:bg-mm-teal hover:text-mm-sand" onClick={hadleFormSubmit}>Añadir registro</button>
     </form>
   )
 }
