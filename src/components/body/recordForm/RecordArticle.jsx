890928774b13da936498ef80ad16674f203219d7
Pythonesa <pythonesamar@gmail.com>
2024-01-19 17:28:15 -0300

    :lipstick: redesign record article

--- src/components/body/recordForm/RecordArticle.jsx
+++ src/components/body/recordForm/RecordArticle.jsx
@@ -8,7 +8,7 @@ export default function RecordArticle ({record, editRecord, deleteRecord}) {
     <article key={record.id} className={`flex flex-col sm:flex-row justify-center items-center ${backgroundColor} rounded shadow shadow-mm text-mm w-11/12 mt-1 mb-2`}>
       <div className="flex flex-col sm:flex-row justify-between items-center p-2 w-full">
         <h2 className="font-bold sm:w-1/2">{record.name}</h2>
-        <p>${record.amount}</p>
+        <p className="grow">${record.amount}</p>
         <p>{record.date}</p>
       </div>
       <div className='flex flex-row justify-center items-center pb-2 sm:pb-0 sm:pr-2'>
